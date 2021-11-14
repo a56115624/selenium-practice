290a0d681506c24ce931e6ea0b5654c4402dd6f3
shane <qaz56115624@gmail.com>
2021-11-14 17:58:50 +0800

    add:ok

--- MachineLearing/icook.py
+++ MachineLearing/icook.py
@@ -10,20 +10,24 @@ import time
 PATH = "C:/Users/admin/Desktop/新增資料夾/chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 driver.get("https://icook.tw/")
-
+#指向search列
 search = driver.find_element_by_name("q")
+#先清空
 search.clear()
+#輸入牛肉
 search.send_keys("牛肉")
+#按下enter
 search.send_keys(Keys.RETURN)
 #此句的意思是,等到某句出現後才執行後續的程式碼
 WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.CLASS_NAME, "browse-title-count"))
 )
-
+#因為全部的titles都有都含同樣的class_name
 titles = driver.find_elements_by_class_name("browse-recipe-name")
 for title in titles:
     print(title.text)
 
 
 time.sleep(5)
+#執行結束後關閉
 driver.close()
